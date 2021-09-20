ab5cc2e8a002bacfa4b48732bcc24116b3da8174
Lucas Fernandez <lucas.fernamdez.lmf11@gmail.com>
2021-09-19 21:31:26 -0300

    hice funcionar el formulario en contacto

--- sender/sender.js
+++ sender/sender.js
@@ -14,18 +14,28 @@ const form = document.getElementById('formu');
 form.addEventListener('submit', (event) => {
     //deter la recarga de la pagina 
     event.preventDefault();
+    sender(nombre.value,telefono.value,email.value,mensaje.value)
+
+    form.reset()
 
 })
 
 //declaro la funcion para enviar
 
 function sender (nombre, telefono, email, mensaje,){
-    email.send({
-        SecureToken: '',//token de seguridad de smtp.js 
-        To: 'correo de destino', //donde va a llegar el mensaje
-        From: 'correo de origen',
+    Email.send({
+        SecureToken: 'ff969e9c-b686-481e-b86b-a247b0132708',//token de seguridad de smtp.js 
+        To: 'lucas.fernamdez.lmf11@gmail.com', //donde va a llegar el mensaje
+        From: 'lucas.fernamdez.lmf11@gmail.com',
         Subject: `${nombre} te envio un mensaje por la web`,
-        Body: 
-
-    })
+        Body: `
+            <p>Nombre: ${nombre}</p>
+            <p>Telefono:${telefono}</p>
+            <p>Email: ${email}</p>
+            <p>Mensaje: ${mensaje}</p>        
+        `
+    }).then(
+        message => swal("Mensaje enviado!", "Nos pondremos en contacto en breve", "success")
+    )
+    .catch(error => swal("Error al eviar", "vuelve a intentar mas tarde", "error"))
 }
